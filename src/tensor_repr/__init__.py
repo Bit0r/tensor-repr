c7aebc355da6e79b354441e13c17e10925346084
Bit0r <nie_wang@outlook.com>
2023-07-22 22:24:03 +0800

    🚀 重构代码

--- src/tensor_repr/__init__.py
+++ src/tensor_repr/__init__.py
@@ -1,32 +1,44 @@
+from collections import OrderedDict
+
 import torch
-from cheap_repr import register_repr
+from cheap_repr import ReprHelper, register_repr
 
 
 @register_repr(torch.Tensor)
-def repr_tensor(tensor: torch.Tensor, helper):
-    rpr = f"""shape={tuple(tensor.shape)} dtype={str(tensor.dtype)[6:]}
-device={tensor.device}
-requires_grad={tensor.requires_grad} grad_fn={tensor.grad_fn}
-count_nan={tensor.isnan().sum().item()} count_inf={tensor.isinf().sum().item()}""" # noqa
-
-    tensor_min, tensor_max = tensor.min().item(), tensor.max().item()
-
-    if tensor.dtype.is_floating_point:
-
-        # 如果是浮点数，就将其四舍五入到小数点后5位
-        tensor_max, tensor_min = round(tensor_max, 5), round(tensor_min, 5)
-
-        # 如果是浮点数，还要计算一些统计量
-        rpr += f''' mean={round(tensor.mean().item(), 5)}
-std={round(tensor.std().item(), 5)}
-25%={round(tensor.quantile(0.25).item(), 5)}
-median={round(tensor.median().item(), 5)}
-75%={round(tensor.quantile(0.75).item(), 5)}'''
-
-    # 获取tensor的最大值和最小值
-    rpr += f' min={tensor_min} max={tensor_max}'
-
-    # 处理一下字符串格式
-    rpr = rpr.replace('\n', ' ').replace(' ', ', ')
-    rpr = f'torch.Tensor({rpr})'
+def repr_tensor(tensor: torch.Tensor, helper: ReprHelper):
+    properties = get_properties(tensor)
+    # 将properties显示为key=value的形式
+    properties = ', '.join(f'{key}={value}'
+                           for key, value in properties.items())
+    rpr = f'torch.Tensor({properties})'
     return rpr
+
+
+# 获取tensor的属性，比如mean, std, min, max等
+def get_properties(tensor: torch.Tensor):
+    properties = OrderedDict([
+        ('shape', tuple(tensor.shape)),
+        ('dtype', str(tensor.dtype)[6:]),
+        ('device', tensor.device),
+        ('requires_grad', tensor.requires_grad),
+        ('grad_fn', tensor.grad_fn),
+        ('count_nan', tensor.isnan().sum().item()),
+        ('count_inf', tensor.isinf().sum().item()),
+    ])
+
+    try:
+        properties['min'] = tensor.min().item()
+        properties['max'] = tensor.max().item()
+        properties['median'] = tensor.median().item()
+        properties['mean'] = tensor.mean().item()
+        properties['std'] = tensor.std().item()
+        properties['25%'] = tensor.quantile(0.25).item()
+        properties['75%'] = tensor.quantile(0.75).item()
+    except RuntimeError:
+        pass
+
+    for key, value in properties.items():
+        if isinstance(value, float | complex):
+            properties[key] = round(value, 5)
+
+    return properties
